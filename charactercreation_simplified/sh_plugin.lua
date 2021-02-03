61dda3b1cee91cdd8217d4e261eb6f78d06e21c2
Rune Knight <30399783+Runian@users.noreply.github.com>
2021-02-03 05:37:49 -0800

    Redone

---
+++ charactercreation_simplified/sh_plugin.lua
@@ -0,0 +1,17 @@
+PLUGIN.name = "Character Creation: Simplified"; 
+PLUGIN.author = "Rune Knight";
+PLUGIN.description = "Simplifies the character creation derma by breaking it down into hooks and a readable table.";
+PLUGIN.license = [[Copyright 2021 Rune Knight
+
+This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.]];
+
+--[[
+	STEAM: https://steamcommunity.com/profiles/76561198028996329/
+	DISCORD: Rune Knight#5972
+]]
+
+--[[
+	This plugin was ultimately created to simplify how the character creation is handled and to move it away from the schema.
+]]
+
+ix.util.Include( "cl_plugin.lua" );
